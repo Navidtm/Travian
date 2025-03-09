ae33c37f50e944206db1ede7665ad92e6dd7b0dd
Navid Talebian <navidt81@gmail.com>
2025-03-10 03:07:42 +0330

    set eslint config as formatter and choose bun for project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,30 +3,46 @@
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  ssr: false,
-  devtools: { enabled: true },
-  devServer: {
-    port: 3001,
-  },
-  modules: [
-    '@pinia/nuxt',
-    '@nuxt/fonts',
-    '@nuxt/icon',
-    '@vueuse/nuxt',
-    '@nuxt/image',
-    '@nuxt/test-utils',
-  ],
-  css: ['~/assets/css/main.css'],
-  vite: {
-    plugins: [tailwindcss()],
-  },
-  runtimeConfig: {
-    id: process.env.PHPSESSID,
-    baseURL: process.env.BASE_URL,
-    username: process.env.COOKUSR,
-    email: process.env.COOKEMAIL,
-    serverType: process.env.SERVER_TYPE,
-    domain: process.env.DOMAIN,
-  },
+    modules: [
+        '@nuxt/eslint',
+        '@pinia/nuxt',
+        '@nuxt/fonts',
+        '@nuxt/icon',
+        '@vueuse/nuxt',
+        '@nuxt/image'
+        // '@nuxt/test-utils',
+    ],
+    devtools: { enabled: true },
+
+    css: ['~/assets/css/main.css'],
+
+    runtimeConfig: {
+        id: process.env.PHPSESSID,
+        baseURL: process.env.BASE_URL,
+        username: process.env.COOKUSR,
+        email: process.env.COOKEMAIL,
+        serverType: process.env.SERVER_TYPE,
+        domain: process.env.DOMAIN
+    },
+
+    devServer: {
+        port: 3001
+    },
+    compatibilityDate: '2024-11-01',
+
+    vite: {
+        plugins: [tailwindcss()]
+    },
+
+    eslint: {
+        config: {
+            stylistic: {
+                semi: true,
+                quotes: 'single',
+                indent: 4,
+                quoteProps: 'as-needed',
+                commaDangle: 'never'
+            }
+        }
+    }
 });
