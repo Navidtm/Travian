503db940792603a6e71d912b16f1508ce02841ba
Navid Talebian <navidt81@gmail.com>
2025-03-20 01:36:05 +0330

    improve UI in FarmCard

--- app/components/cards/FarmCard.vue
+++ app/components/cards/FarmCard.vue
@@ -1,34 +1,43 @@
 <template>
-    <div class="max-w-xs">
+    <div class="max-w-sm w-full">
         <UCard
             v-if="data?.levels"
             variant="solid"
         >
-            <template #header>
-                <div class="text-center text-xl">
-                    مزرعه
-                </div>
-            </template>
-            <template #default>
-                <div class="space-y-4">
-                    <UCard
-                        v-for="(value, key) in groupBy(data.levels, item => `${item.type}`)"
-                        :key
-                        variant="solid"
-                        class="bg-blue-400/10"
-                    >
-                        <div
-                            v-for="{ id, level, type } in value"
+            <div class="text-center text-xl mb-4">
+                مزرعه
+            </div>
+            <div class="space-y-4">
+                <UCard
+                    v-for="(value, key) in groupBy(data.levels, ({ type }) => type)"
+                    :key
+                    variant="solid"
+                    class="bg-blue-400/10 *:py-3"
+                >
+                    <div class="text-center font-bold mb-3">
+                        {{ resourses[key] }}
+                    </div>
+                    <div class="grid grid-cols-4 gap-2 mb-5">
+                        <UButton
+                            v-for="{ id, level } in value"
                             :key="id"
-                            class=""
+                            variant="soft"
+                            @click="upgradeFarm(id)"
                         >
-                            <span>{{ resourses[type] }}</span>
-                            <span>:</span>
-                            <span>{{ level }}</span>
+                            سطح
+                            {{ level }}
+                        </UButton>
+                    </div>
+                    <UButton
+                        class="w-full"
+                        @click="upgradeTypeFarm(key)"
+                    >
+                        <div class="text-center w-full">
+                            ارتقا همه
                         </div>
-                    </UCard>
-                </div>
-            </template>
+                    </UButton>
+                </UCard>
+            </div>
         </UCard>
     </div>
 </template>
@@ -39,4 +48,12 @@ import { resourses } from '~/constants/farm';
 import { useFarm } from '~/stores/useFarm';
 
 const { data } = useFarm();
+
+const upgradeFarm = (id: number) => {
+    console.log(id);
+};
+
+const upgradeTypeFarm = (id: Farm) => {
+    console.log(id);
+};
 </script>
