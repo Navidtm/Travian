86550a07992e8e2d6fa99ce366d42d43d36d354e
Navid Talebian <navidt81@gmail.com>
2025-03-23 17:04:47 +0330

    create schema for upgrade farm handler

---
+++ shared/schemas/farm.schema.ts
@@ -0,0 +1,14 @@
+import { z } from 'zod';
+
+export const farmSchema = z.object({
+    body: z.object({
+        items: z.array(z.object({
+            id: z.number().openapi({ description: 'The farm id' }),
+            type: z.string().openapi({ description: 'The farm type to upgrade ' }),
+            level: z.number().openapi({ description: 'The farm current level' })
+        })),
+        toLevel: z.number().openapi({ description: 'The farm level to upgrade to' })
+    })
+});
+
+export type farmSchemaType = z.infer<typeof farmSchema>;
