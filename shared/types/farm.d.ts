00051c3f23e309ed02bec5f779275f10b2724df0
Navid Talebian <navidt81@gmail.com>
2025-03-23 17:02:38 +0330

    add types for farm

--- shared/types/farm.d.ts
+++ shared/types/farm.d.ts
@@ -3,3 +3,11 @@ export type Resourse = 'iron' | 'wheat' | 'wood' | 'clay' | 'population';
 export type Farm = 'iron' | 'wheat' | 'wood' | 'clay';
 
 export type FarmId = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18;
+
+export type FarmLevel = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20;
+
+export type FarmItem = {
+    id: FarmId;
+    level: FarmLevel;
+    type: Farm;
+};
