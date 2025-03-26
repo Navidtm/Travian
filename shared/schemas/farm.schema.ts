f2e67d7ba3b88f2268250648c893df963166bbdc
Navid Talebian <navidt81@gmail.com>
2025-03-26 17:35:00 +0330

    improve farm schema

--- shared/schemas/farm.schema.ts
+++ shared/schemas/farm.schema.ts
@@ -1,11 +1,12 @@
 import { z } from 'zod';
+import { resourseTypes } from '../constants/farm';
 
 export const farmSchema = z.object({
     body: z.object({
         items: z.array(z.object({
-            id: z.number().openapi({ description: 'The farm id' }),
-            type: z.string().openapi({ description: 'The farm type to upgrade ' }),
-            level: z.number().openapi({ description: 'The farm current level' })
+            id: z.number().int().openapi({ description: 'The farm id' }),
+            type: z.enum(resourseTypes).openapi({ description: 'The farm type to upgrade ' }),
+            level: z.number().int().openapi({ description: 'The farm current level' })
         })),
         toLevel: z.number().openapi({ description: 'The farm level to upgrade to' })
     })
