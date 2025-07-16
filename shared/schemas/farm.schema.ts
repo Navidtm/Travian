1acc678150eef6e6c074539267906d686307de34
Navid Talebian <navidt81@gmail.com>
2025-07-16 15:46:56 +0330

    zod migration

--- shared/schemas/farm.schema.ts
+++ shared/schemas/farm.schema.ts
@@ -1,16 +1,18 @@
-import { z } from 'zod';
-import { resourseTypes } from '../constants/farm';
+import { z } from "zod/v4";
+import { resourseTypes } from "../constants/farm";
 
 export const farmSchema = z.object({
     body: z.object({
-        items: z.array(z.object({
-            id: z.number().int().openapi({ description: 'The farm id' }),
-            type: z.enum(resourseTypes).openapi({ description: 'The farm type to upgrade ' }),
-            level: z.number().int().openapi({ description: 'The farm current level' })
-        })),
-        toLevel: z.number().openapi({ description: 'The farm level to upgrade to' })
-    })
+        items: z.array(
+            z.object({
+                id: z.number().int().meta({ description: "The farm id" }),
+                type: z.enum(resourseTypes).meta({ description: "The farm type to upgrade " }),
+                level: z.number().int().meta({ description: "The farm current level" }),
+            })
+        ),
+        toLevel: z.number().meta({ description: "The farm level to upgrade to" }),
+    }),
 });
 
 export type farmSchemaType = z.infer<typeof farmSchema>;
-export type upgradeFarmSchemaType = farmSchemaType['body'];
+export type upgradeFarmSchemaType = farmSchemaType["body"];
