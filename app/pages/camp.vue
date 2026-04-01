7bc691981ab7f69fba27c43b75f760df9dc61615
Navid Talebian <navidt81@gmail.com>
2026-04-01 10:18:01 +0330

    refactor

---
+++ app/pages/camp.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Adventures from './adventures.vue';
+</script>
+
+<template>
+    <div class="text-sm text-white">
+        <!-- <Academy /> -->
+        <!-- <Blacksmith /> -->
+        <Troops />
+        <Adventures />
+    </div>
+</template>
--- server/utils/helpers/time.ts
+++ server/utils/helpers/time.ts
@@ -4,11 +4,12 @@ import type { Locator, Page } from "playwright-core";
 export const sleep = (ms: number) =>
     new Promise(resolve => setTimeout(resolve, ms));
 
+export const calculateSec = (clock: string) => sum(clock.split(':')
+    .reverse()
+    .map((v, i) => +v * Math.pow(60, i)));
+
 export const getSecFromClock = async (locator: Locator | Page) => {
     const clock = await locator.locator('.clocks').first().innerText();
 
-    return sum(clock.split(':')
-        .reverse()
-        .map((v, i) => +v * Math.pow(60, i))
-    );
+    return calculateSec(clock);
 };
